b75bf711824b60fa762c354e4f11ec7aec17c678
lgtome <seryikotenok232@gmail.com>
2022-04-21 23:30:15 +0300

    upd

--- bundle/popup.js
+++ bundle/popup.js
@@ -1,11 +1,12 @@
 const textArea = document.getElementById('area')
 
 const DEFAULT_TEXT = 'Insert template using **markdown** here!'
-textArea.addEventListener('change',(event) => {
-    chrome.storage.local.set({text: event.target.value || DEFAULT_TEXT})
+textArea.addEventListener('change', (event) => {
+    const text = event.target.value || DEFAULT_TEXT
+    chrome.storage.local.set({ text })
 })
 chrome.storage.local.get('text', (value) => {
-    if(value?.text){
-    textArea.value = value.text
+    if (value?.text) {
+        textArea.value = value.text
     }
 })
